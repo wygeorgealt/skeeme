484acc07e688351125c3f5f51d0e19fe28234c6b
Skeeme Deployer <deploy@skeeme.com>
2026-05-20 11:33:05 +0100

    feat: add support for native PDF parsing in Google Document AI processing

--- app/Services/GoogleDocumentAIService.php
+++ app/Services/GoogleDocumentAIService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Google\Cloud\DocumentAI\V1\Client\DocumentProcessorServiceClient;
 use Google\Cloud\DocumentAI\V1\ProcessRequest;
+use Google\Cloud\DocumentAI\V1\ProcessOptions;
 use Google\Cloud\DocumentAI\V1\RawDocument;
 use Illuminate\Support\Facades\Log;
 
@@ -67,9 +68,14 @@ class GoogleDocumentAIService
 
             $name = $client->processorName($this->projectId, $this->location, $this->processorId);
 
+            $processOptions = new ProcessOptions([
+                'enable_native_pdf_parsing' => true,
+            ]);
+
             $request = new ProcessRequest([
                 'name' => $name,
                 'raw_document' => $rawDocument,
+                'process_options' => $processOptions,
             ]);
 
             $response = $client->processDocument($request);
